597ce2bbd35b97b79bfd889989c586aea69d514c
kimitaWanjohi <kimitawanjohi7923@gmail.com>
2023-09-01 10:15:37 +0300

    Fix imports

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './nameable'
 require './decorator'
-require './capitalize_decorator'
-require './trimmer_decorator'
+require './capitalize'
+require './trimmer'
 require './rental'
 
 class Person < Nameable
@@ -39,14 +39,3 @@ class Person < Nameable
     Rental.new(date, book, self)
   end
 end
-
-person = Person.new(22, name: 'maximilianus')
-person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-
-puts capitalized_trimmed_person.correct_name
