88451a8994530f308c923dc91685c3f501218850
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-09-05 20:12:02 +0200

    Add initialize method and create_new_json method

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative 'classes/student'
 require_relative 'classes/teacher'
 require_relative 'classes/rental'
@@ -8,11 +9,19 @@ class App
   attr_accessor :books, :people, :rentals
 
   def initialize
-    @people = []
-    @books = []
-    @rentals = []
+    created_books_file = create_new_json('books')
+    @books = created_books_file ? JSON.parse(created_books_file.read, create_additions: true) : []
+    created_people_file = create_new_json('people')
+    @people = created_people_file ? JSON.parse(created_people_file.read, create_additions: true) : []
+    created_rent_file = create_new_json('rentals')
+    @rentals = created_rent_file ? JSON.parse(created_rent_file.read, create_additions: true) : []
     @list_elements = ListElements.new(@books, @people, @rentals)
     @creale_elements = CreateElements.new(@books, @people, @rentals)
+  end
+
+  def create_new_json(file_name)
+    File.write("./#{file_name}.json", []) unless File.exist?("./#{file_name}.json")
+    File.open("./#{file_name}.json", 'r')
   end
 
   def exit_app
@@ -45,6 +54,21 @@ class App
       ui.options
       option = gets.chomp
       apply_option(option)
+    end
+  end
+
+  def keeping_data
+    File.open('./people.json', 'w+') do |file|
+      people = JSON.dump(@people)
+      file.write(people)
+    end
+    File.open('./rentals.json', 'w+') do |file|
+      rentals = JSON.dump(@rentals)
+      file.write(rentals)
+    end
+    File.open('./books.json', 'w+') do |file|
+      books = JSON.dump(@books)
+      file.write(books)
     end
   end
 end
